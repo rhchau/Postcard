c3a6d585316f16b5610d2a53ffeea7c75440ff85
RICHARD CHAU <richardchau@RICHARDs-MacBook-Air.local>
2015-01-07 22:05:34 -0800

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //creates a reference to the UI element
+    //IBOutlet - compiler directive
+    //weak pointer
+    //var variable pointer
+    //name of variable
+    // : UILable type
+    //! is optional, unwrapping
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //IBAction - compiler directive for calling function when button is pressed
+    //func - defines function
+    //name of function
+    //sender - is the reference to the button object, it is passed in so it can be accessed within the function.
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false;
+        messageLabel.text = enterMessageTextField.text;
+        enterMessageTextField.text = "";
+        
+        //calls this function to make keyboard go away
+        
+        enterMessageTextField.resignFirstResponder();
+    }
 }
 
